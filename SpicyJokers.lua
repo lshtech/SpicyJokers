b6913b256c7a42faa0b3d4fe4d6fade1d1b7ed27
Richard Dimaria <rdimaria@DESKTOP-7143ERJ.localdomain>
2024-07-04 23:21:10 -0400

    added two new jokers (Business Joker and Void) Along with their art

--- SpicyJokers.lua
+++ SpicyJokers.lua
@@ -2,7 +2,7 @@
 --- MOD_NAME: SpicyJokers
 --- MOD_ID: SpicyJokers
 --- MOD_AUTHOR: [Richard]
---- MOD_DESCRIPTION: This mod adds two new jokers as of right now
+--- MOD_DESCRIPTION: This mod adds 4 new jokers with unique art and abilities
 --- BADGE_COLOUR: C9A926
 
 ----------------------------------------------
@@ -49,6 +49,43 @@ function SMODS.INIT.SpicyJokers()
             cost = 4,
             blueprint_compat = false,
             eternal_compat = true
+
+        },
+        {
+            name = "Business Joker",
+            slug = 'ssj_business_joker',
+            desc = {
+                "Create a {C:attention}Hermit{} {C:tarot}Tarot{}",
+                "if {C:attention}round won{}",
+                "with only {C:chips}1{} hand",
+                "{C:inactive}(Must have room)"
+            },
+            config = {
+                extra = 3
+            },
+            pos = {x = 2, y = 0}, -- POSITION IN SPRITE SHEET
+            rarity = 2,
+            cost = 6,
+            blueprint_compat = true,
+            eternal_compat = false
+
+        },
+        {
+            name = "Void",
+            slug = 'ssj_void',
+            desc = {
+                "When {C:attention}blind{} is selected,",
+                "Create a {C:spectral}Black Hole{} card",
+                "and {C:attention}destroy{} a random joker",
+                "{C:inactive}(Must have room)"
+            },
+            config = {
+            },
+            pos = {x = 3, y = 0}, -- POSITION IN SPRITE SHEET
+            rarity = 2,
+            cost = 6,
+            blueprint_compat = true,
+            eternal_compat = false
 
         }
     }
@@ -98,6 +135,7 @@ function SMODS.INIT.SpicyJokers()
             }
         end
     end
+
     SMODS.Jokers.j_ssj_lucky_seven.calculate = function(self, context)
         if context.before and  context.cardarea == G.jokers then
             seven = false;
@@ -123,7 +161,46 @@ function SMODS.INIT.SpicyJokers()
                 }
             end
         end
+    end
 
+    SMODS.Jokers.j_ssj_business_joker.calculate = function(self, context)
+        if G.GAME.current_round.hands_played == 0 and context.after and G.GAME.chips + hand_chips * mult > G.GAME.blind.chips then
+            if #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+                G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'before',
+                    delay = 0.0,
+                    func = (function()
+                            local card = create_card(nil,G.consumeables, nil, nil, nil, nil, 'c_hermit', 'sup')
+                            card:add_to_deck()
+                            G.consumeables:emplace(card)
+                            G.GAME.consumeable_buffer = 0
+                        return true
+                    end)}))
+                card_eval_status_text(self, 'extra', nil, nil, nil, {message = "Hermit", colour = G.C.PURPLE})
+                end
+        end
+    end
+
+    SMODS.Jokers.j_ssj_void.calculate = function(self, context)
+        if context.setting_blind and not self.getting_sliced and not (context.blueprint_card or self).getting_sliced and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+            local destructable_jokers = {}
+            for i = 1, #G.jokers.cards do
+                if G.jokers.cards[i] ~= self and not G.jokers.cards[i].ability.eternal and not G.jokers.cards[i].getting_sliced then destructable_jokers[#destructable_jokers+1] = G.jokers.cards[i] end
+            end
+            local joker_to_destroy = #destructable_jokers > 0 and pseudorandom_element(destructable_jokers, pseudoseed('void')) or nil
+            if joker_to_destroy and not (context.blueprint_card or self).getting_sliced then 
+                joker_to_destroy.getting_sliced = true
+                G.E_MANAGER:add_event(Event({func = function()
+                    (context.blueprint_card or self):juice_up(0.8, 0.8)
+                    joker_to_destroy:start_dissolve({G.C.PURPLE}, nil, 1.6)
+                    local card = create_card(nil,G.consumeables, nil, nil, nil, nil, 'c_black_hole', 'sup')
+                    card:add_to_deck()
+                    G.consumeables:emplace(card)
+                    G.GAME.consumeable_buffer = 0
+                return true end }))
+            end
+        end
     end
 
     -- DECK FOR TESTING
@@ -144,6 +221,15 @@ function SMODS.INIT.SpicyJokers()
             G.E_MANAGER:add_event(Event({
                 func = function()
                     local card = create_card('Joker', G.jokers, nil, nil, nil, nil, 'j_ssj_lucky_seven', nil)
+                    card:add_to_deck()
+                    G.jokers:emplace(card)
+                    local card = create_card('Joker', G.jokers, nil, nil, nil, nil, 'j_ssj_business_joker', nil)
+                    card:add_to_deck()
+                    G.jokers:emplace(card)
+                    local card = create_card('Joker', G.jokers, nil, nil, nil, nil, 'j_ssj_gun_joker', nil)
+                    card:add_to_deck()
+                    G.jokers:emplace(card)
+                    local card = create_card('Joker', G.jokers, nil, nil, nil, nil, 'j_ssj_void', nil)
                     card:add_to_deck()
                     G.jokers:emplace(card)
                     return true
